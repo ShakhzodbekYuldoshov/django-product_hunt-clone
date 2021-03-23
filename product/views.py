7d71b9d279c97923fa3d89ddad782c5634ddc85f
ShakhzodbekYuldoshov <shahzodyoldoshov10@gmail.com>
2021-03-23 19:02:45 +0500

    home page added

--- product/views.py
+++ product/views.py
@@ -6,10 +6,12 @@ from django.utils import timezone
 
 # Create your views here.
 def home(request):
-    return render(request, 'products/home.html')
+    products = Product.objects
+
+    return render(request, 'products/home.html', {'products': products})
 
 
-@login_required
+@login_required(login_url='accounts/sign_up')
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
